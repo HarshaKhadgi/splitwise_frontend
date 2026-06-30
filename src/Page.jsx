e096022f9ade5f856c0482d8d5104781e67d0a6d
Harsha Khadgi <harshakhadgi28@gmail.com>
2026-06-30 23:00:28 +0530

    gyut7u

--- src/Page.jsx
+++ src/Page.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div>Page in new folder Hi harsha</div>
+    <div>Page in new folder Hi harsha
+      <h1>jnhhybhbnhj</h1>
+    </div>
   )
 }
 
