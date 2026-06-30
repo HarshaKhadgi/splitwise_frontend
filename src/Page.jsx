5a7c07b18f31cd19a4af312637f79ebaf8b805af
Harsha Khadgi <harshakhadgi28@gmail.com>
2026-06-30 23:17:42 +0530

    conflict resolved

--- src/Page.jsx
+++ src/Page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div>Page in new folder Hi harsha Hi ritik AgenticIQ
+    <div>Page in new folder Hi harsha Hi ritik Hello AgenticIQ
       <h1>jnhhybhbnhj</h1>
     </div>
   )
